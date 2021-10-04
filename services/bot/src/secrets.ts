294f8c4249ee0bded6e3cb14157d082a780b5037
Ava Thorn <avatheavian@gmail.com>
2021-10-03 20:45:06 -0400

    update secret path for local runs

--- services/bot/src/secrets.ts
+++ services/bot/src/secrets.ts
@@ -14,6 +14,6 @@ export const GET = (secret: string): string => {
     if (['production', 'development'].includes(process.env.NODE_ENV || '')) {
         return fs.readFileSync(`/run/secrets/${secret}`, 'utf8').trim();
     } else {
-        return fs.readFileSync(`../../${secret}`, 'utf8').trim();
+        return fs.readFileSync(`../../secrets/local/${secret}`, 'utf8').trim();
     }
 };
