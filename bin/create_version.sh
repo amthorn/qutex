e67e8ee4ca8e57106454fd7493679fd7023c7469
Ava Thorn <avatheavian@gmail.com>
2021-10-29 05:11:55 -0400

    fixed ci

---
+++ bin/create_version.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+today=$(date +"%b %d, %Y")
+
+QUTEX_RELEASE_DATE="$today"
+QUTEX_VERSION=$1
+
+echo "
+Release Date: ${QUTEX_RELEASE_DATE}
+Version: ${QUTEX_VERSION}
+"
+if [[ -z $2 ]]; then
+    read -p "Press enter to continue"
+fi;
+
+echo "QUTEX_RELEASE_DATE=${QUTEX_RELEASE_DATE}
+QUTEX_VERSION=${QUTEX_VERSION}" > .version.env
--- bin/deploy_stack.sh
+++ bin/deploy_stack.sh
@@ -7,22 +7,9 @@ else
     echo "Version number found: $1"
 fi
 
-today=$(date +"%b %d, %Y")
-
 # Deploy the docs to docs.qutexbot.com
 mkdocs gh-deploy
 
-QUTEX_RELEASE_DATE="$today"
-QUTEX_VERSION=$1
-
-echo "
-Release Date: ${QUTEX_RELEASE_DATE}
-Version: ${QUTEX_VERSION}
-"
-
-read -p "Press enter to continue"
-
-echo "QUTEX_RELEASE_DATE=${QUTEX_RELEASE_DATE}
-QUTEX_VERSION=${QUTEX_VERSION}" > .version.env
+./create_version.sh "$1"
 
 env QUTEX_VERSION="${QUTEX_VERSION}" docker --context qutex stack deploy --with-registry-auth qutex -c docker-compose.yml
